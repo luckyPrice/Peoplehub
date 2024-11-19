5930b5e8f5eae755b400c7c44aea3f2fbbefe889
luckyPrice <luckyprice1103@naver.com>
2024-11-20 01:56:38 +0900

    React Routing implementaion

--- EmployeeMS/src/App.jsx
+++ EmployeeMS/src/App.jsx
@@ -2,12 +2,19 @@ import { useState } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Login from './Components/login'
+import Dashboard from './Components/Dashboard'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
 
   return (
-    <><Login/></>
-  )
+    <BrowserRouter>
+    <Routes>
+      <Route path='/adminlogin' element={<Login />}></Route>
+      <Route path='/dashboard' element={<Dashboard />}></Route>
+    </Routes>
+    </BrowserRouter>
+    )
 }
 
 export default App
--- Server/index.js
+++ Server/index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(cors({
     origin: ["http://localhost:5173"],
     methods: ['GET', 'POST', 'PUT'],
-    creditionals: true,
+    credentials: true,
 }))
 app.use(express.json())
 app.use('/auth', adminRouter)
